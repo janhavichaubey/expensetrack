065a44da304dbcfb9ff1c54936c988287fea2704
aaryanchaubey <aaryanchaubey693@gmail.com>
2023-11-04 09:30:52 +0530

    second

---
+++ src/components/TransactionList.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { Transaction } from './transaction';
+import { GlobalContext } from './GlobalState';
+
+export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+    return (
+        <>
+            <h3>History</h3>
+            <ul className="list">
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+
+
+
+
+            </ul>
+        </>
+
+    )
+}
